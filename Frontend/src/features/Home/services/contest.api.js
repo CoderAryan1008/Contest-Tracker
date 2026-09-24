16f876f93f514196f92b4e2814c6aebc4c0f0659
AryanJain1008 <aryanpict2025@gmail.com>
2026-09-24 21:40:23 +0530

    Changed the Env variables

--- Frontend/src/features/Home/services/contest.api.js
+++ Frontend/src/features/Home/services/contest.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = (import.meta.env.VITE_API_URL || "http://localhost:3000").replace(
+const BASE_URL = ("https://contest-tracker-jmf8.onrender.com" || "http://localhost:3000").replace(
   /\/$/,
   "",
 );
